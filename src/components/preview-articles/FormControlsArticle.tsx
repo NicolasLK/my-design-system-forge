cb85439df2646c97e3c2a121fe4fc09c5803ab4e
NicolasLK <nicolas.kaminski.2001@gmail.com>
2026-01-22 17:38:20 +0000

    refactor: transform the components names of ControlsArticle into an variable

--- src/components/preview-articles/FormControlsArticle.tsx
+++ src/components/preview-articles/FormControlsArticle.tsx
@@ -3,6 +3,40 @@ import type { FormControlsComponentPreviewKey } from '@/typings/preview-articles
 import { useState } from 'react';
 import { FormControlsPreview } from './form-controls/FormControlsPreview';
 
+const componentList: Array<{
+    id: number;
+    value: FormControlsComponentPreviewKey;
+}> = [
+    {
+        id: 1,
+        value: 'button',
+    },
+    {
+        id: 2,
+        value: 'input',
+    },
+    {
+        id: 3,
+        value: 'textarea',
+    },
+    {
+        id: 4,
+        value: 'switch',
+    },
+    {
+        id: 5,
+        value: 'select',
+    },
+    {
+        id: 6,
+        value: 'slider',
+    },
+    {
+        id: 7,
+        value: 'simple-date-picker',
+    },
+];
+
 export const FormControlsArticle = () => {
     const [active, setActive] =
         useState<FormControlsComponentPreviewKey>('button');
@@ -20,48 +54,15 @@ export const FormControlsArticle = () => {
                 </p>
 
                 <div className="form-controls-article-grid">
-                    <button
-                        className="form-controls-article-card"
-                        onClick={() => setActive('button')}
-                    >
-                        button
-                    </button>
-                    <button
-                        className="form-controls-article-card"
-                        onClick={() => setActive('input')}
-                    >
-                        input
-                    </button>
-                    <button
-                        className="form-controls-article-card"
-                        onClick={() => setActive('textarea')}
-                    >
-                        textarea
-                    </button>
-                    <button
-                        className="form-controls-article-card"
-                        onClick={() => setActive('switch')}
-                    >
-                        switch
-                    </button>
-                    <button
-                        className="form-controls-article-card"
-                        onClick={() => setActive('select')}
-                    >
-                        select
-                    </button>
-                    <button
-                        className="form-controls-article-card"
-                        onClick={() => setActive('slider')}
-                    >
-                        slider
-                    </button>
-                    <button
-                        className="form-controls-article-card"
-                        onClick={() => setActive('simple-date-picker')}
-                    >
-                        simple-date-picker
-                    </button>
+                    {componentList.map((comp) => (
+                        <button
+                            key={comp.id}
+                            className="form-controls-article-card"
+                            onClick={() => setActive(comp.value)}
+                        >
+                            {comp.value}
+                        </button>
+                    ))}
                 </div>
 
                 {active && <FormControlsPreview component={active} />}
